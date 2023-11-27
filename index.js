67d136fb80023a496cfad5f2b96aed613d5c630a
Vishwas JS <vishwasjs2806@gmail.com>
2023-11-27 12:25:40 +0530

    new updates

--- index.js
+++ index.js
@@ -163,10 +163,10 @@ var swiper2 = new Swiper(".swiper-2", {
   spaceBetween: 30,
   centeredSlides: true,
   loop: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
